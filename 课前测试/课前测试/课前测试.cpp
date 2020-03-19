fc4dc79bc54db6e584180e7260d519457de40f1d
niuhongwei <nhw1013@outlook.com>
2020-03-19 14:46:37 +0800

    第二节作业

--- 课前测试/课前测试/课前测试.cpp
+++ 课前测试/课前测试/课前测试.cpp
@@ -1,5 +1,11 @@
-﻿#include <stdio.h>
-int main(void) {
-	printf("Hello world!\nHello cute C!");
-		return 0;
+﻿#include<stdio.h>
+int main(void)
+{
+	int ref[] = { 8, 4, 0, 2 };
+	int* ptr;
+	int index;
+	for (index = 0, ptr = ref; index < 4; index++, ptr++) 
+		printf("%d %d\n", ref[index], *ptr);
+	
+	return 0;
 }
