61b498570b6f6262f80677e6150017985638d28b
niuhongwei <nhw1013@outlook.com>
2020-03-24 16:43:07 +0800

    24日C语言

---
+++ 课前测试/3.2/3.2.cpp
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main() {
+	float a,b,s;
+	scanf("%f", &a);
+	b = 3.14159;
+	s = b * a * a;
+	printf("r=%.2f\narea=%.2f", a, s);
+	return 0;
+}
---
+++ 课前测试/课前测试第二章/课前测试第二章.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main(void){
+	int i;
+	float x = 3.6;
+	i = (int)x / 6;
+	printf("x=%f,i=%d", x,i);
+	return 0;
+
+
+
+}
