22cb86380a8a3e020499622c08701973f080b907
niuhongwei <nhw1013@outlook.com>
2020-03-27 01:08:21 +0800

    第五节课

---
+++ 课前测试/4.1/4.1.cpp
@@ -0,0 +1,9 @@
+﻿#include <math.h>
+#include <stdio.h>
+int main() {
+    double a, b, c;
+    scanf("%lf%lf", &a, &b);
+    c = sqrt(a * a + b * b);
+    printf("%.2lf\n", c);
+    return 0;
+}
---
+++ 课前测试/4.2/4.2.cpp
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main()
+{
+	char c1, c2;
+	scanf("%c", &c1);
+	c2 = c1 + 32;
+	printf("%c,%d", c2, c2);
+	return 0;
+}
