3eaec1946c9abd44f5df22e32a654f4d527314bd
niuhongwei <nhw1013@outlook.com>
2020-05-17 18:52:39 +0800

    选择排序

--- 排序算法/selectionSort.c
+++ 排序算法/selectionSort.c
@@ -20,14 +20,14 @@ void selectionSortInDescendingOrder(int array[], int length) {
 void selectionSortInAscendingOrder(int array[], int length) {
 	for (int i = 0; i < length - 1; i++) {
 		int minIndex=i;
-		for (int j = 1; j < length; j++) {
+		for (int j = i+1; j < length; j++) {
 			if (array[j] < array[minIndex]) {
 				minIndex = j;
 			}
 		}
 		int temp = array[i];
 		array[i] = array[minIndex];
-		array[minIndex] = array[i];
+		array[minIndex] = temp;
 	}
 }
 
